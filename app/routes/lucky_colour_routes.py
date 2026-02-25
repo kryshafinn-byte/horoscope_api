bee611b69d30c126ccf67f261ceefd96bad11f80
kryshafinn-byte <krysha.finn@gmail.com>
2026-02-25 19:11:54 +0000

    Shortened docstring to be easier and cleaned up my lucky_colour routegit add .

--- app/routes/lucky_colour_routes.py
+++ app/routes/lucky_colour_routes.py
@@ -6,31 +6,9 @@ def make_lucky_colours(app):
     @app.route('/lucky-colour/<name>', methods=['GET'])
     def get_lucky_colours(name):
         """
-        Get Lucky Colours for your Zodiac Sign (or someone else's!)
-        ---
-        tags:
-          - Lucky Colours
-        parameters:
-          - name: name
-            in: path
-            type: string
-            required: true
-            description: The zodiac sign to look up
-        responses:
-          200:
-            description: A list of lucky colours for the given sign
-            examples:
-              application/json:
-                {
-                  "sign": "Aries",
-                  "lucky_colours": ["Red", "Scarlet", "Fire Orange"]
-                }
-          404:
-            description: Sign not found
-            examples:
-              application/json:
-                {"error": "Stars can't find those lucky colours of yours!"}
+        Returns the lucky colours for a zodiac sign.
         """
+
         db = app.get_db_connection()
         cursor = db.cursor(dictionary=True)
 
